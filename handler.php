f81f83da6cc84b9b46c1ec6416eed9cc121d9b5a
Mikhail <genresnoiz@gmail.com>
2018-08-22 15:48:20 +0300

    make it better

--- handler.php
+++ handler.php
@@ -47,7 +47,7 @@ switch ($data->type) {
 
 		$request_params = "null";
 		
-		if ($mysqli_query($link, $sql)) === TRUE) {
+		if (mysqli_query($link, $sql)) === TRUE) {
 			$request_params = array(
 			'user_id' => $data->object->user_id,
 			'message' => 'Количество изменено на: '.$bodyText,
@@ -57,7 +57,7 @@ switch ($data->type) {
 		} else {
 			$request_params = array(
 			'user_id' => $data->object->user_id,
-			'message' => 'Ошибка: '.$link->error,
+			'message' => 'Ошибка: '.mysqli_error($link),
 			'access_token' => $token,
 			'v' => '5.69'		
 			);
